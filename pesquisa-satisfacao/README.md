a72e935b8226ae2e52d7935a538f309c425e4e04
Rosana <ID+seuusuario@users.noreply.github.com>
2026-09-24 23:08:35 -0300

    feat: 1 versão

---
+++ pesquisa-satisfacao/README.md
@@ -0,0 +1,12 @@
+# Pesquisa de satisfação  😄
+
+Este é um projeto desenvolvido com a linguagem Python.
+Neste projeto faremos uma pesquisa com 50 pessoas, iniciando com dados simples como: nome, idade e a opnião relacionada ao atendimento, sendo 1 - excelente, 2 - bom e 3 - ruim.
+Ao final da pesquisa teremos uma soma das quantidades de opniões: **Excelente **😄 e **Ruim **😢.
+
+Neste programa estamos utilizando estrutura de repetição FOR e de decisão if e elif.
+
+
+![Python](https://img.shields.io/badge/Python-3776AB?style=for-the-badge&logo=python&logoColor=white)
+![GitHub](https://img.shields.io/badge/GitHub-100000?style=for-the-badge&logo=github&logoColor=white)
+![VS Code](https://img.shields.io/badge/VS%20Code-007ACC?style=for-the-badge&logo=visual-studio-code&logoColor=white)
---
+++ pesquisa-satisfacao/app.py
@@ -0,0 +1,24 @@
+#Importa a biblioteca do Windows e limpa a tela com o cls se for mac ou linux, utiliza o clear
+import os
+os.system ("cls" if os.name == "nt" else "clear")
+
+soma = 0.0
+cont_excelente = 0
+cont_ruim = 0
+
+#entrada de dados: nome, idade e opnião, dentro da estrutura FOR para limitar a pesquisa em 50.
+for i in range(1,4):   
+    nome = input("Digite seu nome: ")
+    idade = int(input("Digite sua idade: "))
+    opiniao = int (input(" DIgite 1-EXCELENTE, 2-BOM e 3-RUIM: "))
+
+# Estruturas de decisão para contar as opiniões excelente e ruim.
+    if opiniao == 1:
+        cont_excelente += 1
+    elif opiniao == 3:
+        cont_ruim += 1   
+
+#exibindo a quantidade de cada opnião        
+print(f"Quantidade de opnião EXCELENTE é = {cont_excelente} ")
+print(f"Quantidade de opnião RUIM é = {cont_ruim}")   
+print ("****Obrigado por participar da pesquisa!****".center (50))
